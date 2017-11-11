2703147b03bba2ba4e25b923ba8c506e26a9ec55
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-11-11 10:58:32 -0500

    first and last commit

---
+++ FizzBuzz.js
@@ -0,0 +1,36 @@
+﻿var printMe = document.getElementById("something");
+
+var myButton = document.getElementById("btn");
+
+var results = fizzBuzz();
+
+myButton.onclick = function () {
+    printMe.innerHTML = results;
+}
+
+function fizzBuzz() {
+
+    var returnValue = "";
+
+    for (var i = 1; i <= 100; i++) {
+        if (i % 15 === 0) {
+
+            returnValue += "FizzBuzz";
+        }
+        else if (i % 3 === 0) {
+
+            returnValue += "Fizz";
+        }
+        else if (i % 5 === 0) {
+
+            returnValue += "Buzz";
+        }
+        else {
+
+            returnValue += i;
+
+        }
+        returnValue += "<br/>"
+    }
+    return returnValue;
+}
